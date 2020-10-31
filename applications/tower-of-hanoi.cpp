f0267b6b0c675de8c8b756105ddba6e4e96f0dca
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2020-10-31 22:20:50 +0200

    Create tower-of-hanoi.cpp

---
+++ applications/tower-of-hanoi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void towerOfHanoi(int k, char source, char dest, char temp) {
+    if (k == 1) {
+        std::cout << "Move one disk from " << source << " to " << dest << std::endl;
+        return;
+    }
+
+    hanoi(k - 1, source, temp, dest); // Move k-1 disks to temp
+    hanoi(1, source, dest, temp);     // Move 1 disk to dest
+    hanoi(k - 1, temp, dest, source); // Move k-1 to dest
+}
+
+int main() {
+    towerOfHanoi(5, 'rod_A', 'rod_B', 'rod_C');
+
+	  return 0;
+}
