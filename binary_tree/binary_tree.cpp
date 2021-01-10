1b04ba8283251372c304415e9b4f78189b31f930
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2021-01-10 21:29:31 +0200

    Fix nullptr issues

--- binary_tree/binary_tree.cpp
+++ binary_tree/binary_tree.cpp
@@ -285,7 +285,10 @@ template <typename T>
 BinTreePosition<T> BinTree<T>::copy(BinTreePosition<T> pos) {
     // Дървото е празно
     if (pos) {
-        root = new BinTreeNode<T>(*pos, *copy(-pos).ptr, *copy(+pos).ptr);
+        BinTreeNode<T>* left = -pos ? *copy(-pos).ptr : nullptr;
+        BinTreeNode<T>* right = +pos ? *copy(+pos).ptr : nullptr;
+
+        root = new BinTreeNode<T>(*pos, left, right);
     }
     return rootPos();
 }
